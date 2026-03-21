dfb7b9c9cc21ce093a5a0f0a6760bc6fd4f40ee0
maxim <bozzhik@gmail.com>
2026-03-22 00:16:06 +0200

    add `tailwindcss` and `shadcn/ui` for ui
    – https://tailwindcss.com/docs/installation/framework-guides/nextjs
    – https://ui.shadcn.com/docs/installation/next

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,6 +2,8 @@ export {metadata} from '@/lib/layout-config'
 import {geistSans, geistMono} from '@/lib/layout-config'
 import './globals.css'
 
+import {cn} from '@/lib/utils'
+
 import YandexMetrika from '~/global/analytics'
 
 export default function RootLayout({
@@ -10,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="ru">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="ru" className={cn([geistSans.variable, geistMono.variable], 'dark scroll-smooth')}>
+      <body>
         {children}
 
         {process.env.NODE_ENV === 'production' && <YandexMetrika />}
