18391ae5744b629559454a57dc0dc2ee6264bf6e
maxim <bozzhik@gmail.com>
2026-03-27 15:23:04 +0200

    create `Container` global component

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import {hello} from '@repo/shared'
-
 import {useQuery, api} from '@/lib/convex'
+
+import Container from '~/global/container'
 
 export default function IndexPage() {
   const demo = useQuery(api.tables.demo.getDemo)
 
   console.log(hello('web'))
 
-  return <main>{hello(demo?.[0]?.username ?? 'mom')}</main>
+  return <Container>{hello(demo?.[0]?.username ?? 'mom')}</Container>
 }
